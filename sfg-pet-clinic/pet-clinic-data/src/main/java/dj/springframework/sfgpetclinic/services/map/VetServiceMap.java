f1186998598bde02ecdd45e00c4f9d2a43cc988d
DJDrama <putzzang@naver.com>
2023-03-21 00:18:34 +0900

    #21.Load Bootstrap Data on Startup. Closes #21.

--- sfg-pet-clinic/pet-clinic-data/src/main/java/dj/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/dj/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,11 +2,11 @@ package dj.springframework.sfgpetclinic.services.map;
 
 
 import dj.springframework.sfgpetclinic.model.Vet;
-import dj.springframework.sfgpetclinic.services.CrudService;
+import dj.springframework.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
